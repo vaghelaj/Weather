5f29488b7b12a81df21076b8e4f93201b8bfcd06
jayeshvaghela <vaghelaj@sheridancollege.ca>
2017-11-13 18:14:14 -0500

    ES6 & Error Handling
    Added error handling and also changed to ES6 arrow functions.

--- main.js
+++ main.js
@@ -10,22 +10,37 @@ var weatherResponse;
 var request = new XMLHttpRequest();
 //Setting up a connection with the ip info, which fetchs the user's location.
 request.open('GET', 'https://ipinfo.io/json', true);
-request.onload = function() {
 
+request.onload = () => {
+  //Checking the API request status for ipinfo.
+  if(request.status >= 200 && request.status < 400) {
     locationResponse = JSON.parse(request.responseText);
     //Using the fetched location for getting the current weather information.
     request.open('GET', 'https://api.openweathermap.org/data/2.5/weather?q=%27'+locationResponse.city+'+%27&appid=f5ca140b6db0de13daabb40571994509&units=metric', true);
-    request.onload = function () {
+    request.onload = () => {
+      //Checking the API request status for openweathermap.
+      if(request.status >=200 && request.status < 400) {
         weatherResponse = JSON.parse(request.responseText);
         renderInformation(locationResponse, weatherResponse)
+      }
     };
+    //Printing "Something Happen!" in the console when an error occurs.
+    request.onerror = () => {
+      console.log("Something Happen!");
+    }
     request.send();
+  }
 };
+
+//Printing "Something Happen!" in the console when an error occurs.
+request.onerror = () => {
+  console.log("Something Happen!");
+}
 request.send();
 
 
 //This function renders all the inner HTML of the elements.
-function renderInformation(serverLocationResponse, serverWeatherResponse) {
+let renderInformation = (serverLocationResponse, serverWeatherResponse) => {
     city.innerHTML = serverLocationResponse.city;
     region.innerHTML = serverLocationResponse.region.substring(0,2).toUpperCase();
     temperature.innerHTML = Math.floor(serverWeatherResponse.main.temp);
